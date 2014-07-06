421477e5ea666e4fdc971df645eeddcc9c595257
Andelf <andelf@gmail.com>
2014-07-06 20:49:33 +0800

    update key config for ac

--- init.el
+++ init.el
@@ -144,6 +144,7 @@
   ;; (add-hook 'post-self-insert-hook
           ;;;  'swift-mode:indent-on-parentheses)
   (local-set-key (kbd "{") 'my-swift-brace)
+  (set (make-local-variable 'tab-width) 2)
   (set (make-local-variable 'electric-indent-chars) '(?\n ?\: ?\{)))
 
 (add-hook 'swift-mode-hook 'my-swift-mode-hook)
@@ -244,6 +245,9 @@
 (define-key ac-completing-map "\t" 'ac-complete)
 ;;; Ignore case if completion target string doesn't include upper characters
 (setq ac-ignore-case 'smart)
+
+(define-key ac-complete-mode-map "<return>"   'nil)
+(define-key ac-complete-mode-map "RET"        'nil)
 
 
 ;;; xcscope
